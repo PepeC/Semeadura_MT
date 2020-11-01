598a938f09703fc1b02afa4cb3842d53bc2f8006
Jose Clavijo <jose.clavijo@thomsonreuters.com>
2020-11-01 22:33:21 +0000

    first model!

--- 1_get_data.R
+++ 1_get_data.R
@@ -1,6 +1,7 @@
 ###
-# Get Data: bring in data from IMEA (Mato Grosso's farmer association: http://www.imea.com.br/imea-site/relatorios-mercado)
-#Data was supplied by Cleiton Gauer at IMEA, and contians sowing progress for corn, soy and cotton since the 2008/9 season
+# Get Data: bring in data from IMEA (http://www.imea.com.br/imea-site/relatorios-mercado)
+# Data was supplied by Cleiton Gauer at IMEA, and contians sowing progress for corn, 
+# soy and cotton since the 2008/9 season
 ###
 
 library(tidyverse)
@@ -11,4 +12,4 @@ library(nlme)
 library(broom)
 
 #Import sowing dates
-semeadura_mt <- read_csv("~/Google Drive/Brazil_outlook_deforestation/S204_Semeadura_v3.csv")
+semeadura_mt <- read_csv("./S204_Semeadura_v3.csv")
--- 2_data_analysis.R
+++ 2_data_analysis.R
@@ -1,6 +1,9 @@
 library(nlme)
 library(broom)
 library(tidyverse)
+library(minpack.lm)
+
+source("1_get_data.R")
 
 #wrangle semeadura data
 semeadura_mt_w <- semeadura_mt %>%
@@ -20,15 +23,7 @@ semeadura_mt_w <- semeadura_mt %>%
   mutate(doy_c = ifelse(year>min_year, doy + (365-yday(sow_start)), doy - yday(sow_start))) 
 
 
-semeadura_mt_w %>%
-  filter(crop == "soy") %>%
-  filter(week > 10) %>%
-  ggplot(aes(doy, val)) + 
-  geom_line(aes(colour = season)) + 
-  facet_wrap(~macroregion)
-
-
-#Pace by end of OCtober in MT
+# Take a look at the data: Pace by end of October in MT
 semeadura_mt_w %>%
   filter(crop == "soy") %>%
   filter(month == 10 ) %>%
@@ -36,22 +31,34 @@ semeadura_mt_w %>%
   group_by(year) %>%
   filter(doy == max(doy))
 
+
+#Take a look at the data: Plot of sowing pace by district
 semeadura_mt_w %>%
   filter(crop == "soy") %>%
-  arrange(crop, season, macroregion, val) %>%
-  group_by(year, macroregion) %>%
-  mutate(week.prog = val - lag(val)) %>%
-  filter(macroregion == "Mato Grosso") %>%
-  filter(week.prog > 20)
+  filter(week > 10) %>%
+  ggplot(aes(doy, val)) + 
+  geom_line(aes(colour = season)) +
+  xlab("Day fo Year") + ylab("Sowing progress (%)") +
+  facet_wrap(~macroregion) + ggtitle("Soy sowing pace in Mato Grosso (2008/09 - 2019/20")
+
+# How do we model this? Logistic regression!
+# We use the minpack.lm package to fit non-0linear regressions to a 
+# form of the logistic function described in Pinheiro and Bates (in package nlme)
+# The xmid parameter will be key so we can compare when 50% of sowings are complete.
+
 
 #Test to get parameter starting value ideas
 model_sem <- semeadura_mt_w %>%
   filter(crop == "soy" & macroregion == "Nordeste" & year == 2016)
 
-gnls(val ~ SSlogis(doy_c, Asym, xmid, scal), model_sem)
+model_test <- gnls(val ~ SSlogis(doy_c, Asym, xmid, scal), model_sem)
 
+#Now try
+model_sem_nest <- semeadura_mt_w %>%
+  group_by(crop, macroregion, season) %>%
+nest()
 #Now run with do-catch
-model_sem <- semeadura_mt_w %>%
+model_sem2 <- semeadura_mt_w %>%
   group_by(crop, macroregion, season) %>%
   do( model = tryCatch(nlsLM(val ~ Asym/(1 + exp((xmid - doy_c)/scal)), 
                              start = list( Asym = 102, xmid = 53, scal = 8),
@@ -60,10 +67,69 @@ model_sem <- semeadura_mt_w %>%
                              control= c(maxiter=100),
                              data=.)))  
 
-augment(model_sem, model) %>%
+augment(model_sem2, model) %>%
   filter(macroregion == "CentroSul" & crop == "corn") %>%
   ggplot(aes(doy_c, val)) + geom_point() + geom_line(aes(doy_c, .fitted)) + facet_wrap(~season)
 
 
-tidy(model_sem, model)
+tidy(model_sem2, model) %>%
+  filter(term == "xmid") %>%
+  select(crop:std.error) %>%
+  pivot_wider(names_from = c(crop,term), values_from = c(estimate, std.error)) %>%
+  ggplot(aes(estimate_soy_xmid,estimate_corn_xmid)) + geom_point(aes(colour = season)) +
+  facet_wrap(~macroregion) + theme_bw()
+  
+#Define this function to find the days after sowing related to any % completion
+findInt <- function(model, value) {
+  function(x) {
+    predict(model, data.frame(doy_c = x), type="response") - value
+  }
+}
 
+#Define days to test
+prct_sowing <- 75
+
+#now calculate for each crop x macroregion x year combo
+model_sem3 <- left_join(model_sem_nest, model_sem2) %>%
+  group_by(crop, macroregion, season) %>%
+  mutate(
+    root_75 = map2(data, model, ~uniroot(findInt(.y, prct_sowing), range(.x$doy_c))$root)
+  )
+
+unnest(model_sem3, root_75) 
+
+model_sem_roots<- model_sem3 %>% 
+  unnest(root_75) %>%
+  select(crop, season, macroregion, root_75)
+
+#Now try plot again, but this time with the 75% completion for each one
+model_sem_4 <- model_sem2 %>%
+  tidy(model) %>%
+  filter(term == "xmid") %>%
+  select(crop:std.error) %>% 
+  left_join(model_sem_roots) %>%
+  pivot_wider(names_from = c(crop,term), values_from = c(estimate, std.error, root_75))
+  
+#now plot
+  model_sem_4 %>%
+  ggplot(aes(root_75_soy_xmid, estimate_corn_xmid)) + geom_point(aes(colour = season)) +
+  facet_wrap(~macroregion) + theme_bw()
+
+#Now do a quick regression just to get an idea
+
+  model_sem_4b <-  model_sem_4 %>% filter(!macroregion == "Mato Grosso")
+  
+model_lm_75 <- lm(estimate_corn_xmid ~ macroregion + estimate_soy_xmid, data = model_sem_4b)
+
+glance(model_lm_75)
+
+augment(model_lm_75, newdata = model_sem_4b) %>%
+  ggplot(aes(estimate_soy_xmid, estimate_corn_xmid)) + 
+  geom_point(aes(colour = season)) +
+  geom_line(aes(estimate_soy_xmid, .fitted)) +
+  geom_ribbon(aes(ymin=.fitted - 2*.se.fit, ymax=.fitted + 2*.se.fit), linetype=2, alpha=0.5) +
+  facet_wrap(~macroregion) + theme_bw()
+
+  
+  
+  
